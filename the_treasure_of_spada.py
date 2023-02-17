fb2a3b31651381948349a0af398a5dd68e3d27f6
Taís Dias <taisayana@hotmail.com>
2023-02-17 16:57:06 +0000

    This challenge is to create a game, using repetition structures and creativity. I took, as inspiration, the book "Le Comte de Monte-Cristo", by Alexandre Dumas.

--- the_treasure_of_spada.py
+++ the_treasure_of_spada.py
@@ -1,3 +1,10 @@
+# The challenge was to create a game, giving alternatives and, depending on the choice, it would have a consequence, positive or negative.
+# I took the opportunity to reflect on this... every choice we make has consequences... my choice for technology wasn't very obvious to me,
+# but I discovered a treasure in this area... I found myself again!
+# This is the tip I leave: not always the most obvious path will guide you to a better place. Persist and focus!
+
+
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
